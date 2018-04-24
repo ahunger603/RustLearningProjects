0cac32997d7238a06827137a0c5987abb4f7e3c0
Ashton Hunger <ahunger@pdx.edu>
2018-04-24 14:53:57 -0700

    exercise complete

--- Project8_exercism_proverb/src/lib.rs
+++ Project8_exercism_proverb/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
-}
+pub fn build_proverb(list: Vec<&str>) -> String {
+    let mut proverb: Vec<String> = Vec::new();
+    if list.len() > 0 {
+        let start = list[0];
+        let mut last = start;
+        for word in list.iter().skip(1) {
+            proverb.push(format!("For want of a {} the {} was lost.", last, word));
+            last = word;
+        }
+        proverb.push(format!("And all for the want of a {}.", start));
+    }
+    proverb.join("\n")
+}
+
