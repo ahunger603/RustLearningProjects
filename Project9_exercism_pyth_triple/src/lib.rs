568eea53f8d1847bd85ee1795f00774a3126fef6
Ashton Hunger <ahunger@pdx.edu>
2018-04-24 15:34:08 -0700

    exercise complete

--- Project9_exercism_pyth_triple/src/lib.rs
+++ Project9_exercism_pyth_triple/src/lib.rs
@@ -1,3 +1,17 @@
-pub fn find() -> Option<u32> {
-    unimplemented!();
-}
+const TARGET: u32 = 1000;
+pub fn find() -> Option<u32> {
+    for a in 1..TARGET-1 {
+        for b in a..TARGET-1 {
+            if a + b < TARGET {
+                let c = ((a*a + b*b) as f64).sqrt();
+                if c.floor() == c {
+                    if a + b + c as u32 == TARGET {
+                        return Some(a * b * c as u32);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
