181832ebb0dd8b1e08ac8b9d18d9c779be3998c1
Ashton Hunger <ahunger@pdx.edu>
2018-04-23 23:52:32 -0700

    exercise complete

--- Project7_excercism_bob/src/lib.rs
+++ Project7_excercism_bob/src/lib.rs
@@ -1,4 +1,46 @@
-pub fn reply(message: &str) -> &str {
+#[derive(PartialEq)]
+enum MessageType {
+    Silence = 0,
+    Symbols = 1,
+    Normal = 2,
+    Shouting = 3
+}
 
+#[derive(PartialEq)]
+enum Punctuation {
+    None = 0,
+    Statement = 1,
+    Exclaimation = 2,
+    Question = 3
+}
+
+pub fn reply(message: &str) -> &str {
+    let mut message_interpretation : (MessageType, Punctuation) = (MessageType::Silence, Punctuation::None);
+    for c in message.chars() {
+        match c {
+            'A' ... 'Z' => {
+                if message_interpretation.0 == MessageType::Silence || message_interpretation.0 == MessageType::Symbols {
+                    message_interpretation = (MessageType::Shouting, Punctuation::None)
+                }
+            },
+            ' ' | '\t' | '\r' | '\n' => {},
+            '.' => message_interpretation.1 = Punctuation::Statement,
+            '!' => message_interpretation.1 = Punctuation::Exclaimation,
+            '?' => message_interpretation.1 = Punctuation::Question,
+            'a' ... 'z' => message_interpretation = (MessageType::Normal, Punctuation::None),
+            _ => {
+                if message_interpretation.0 == MessageType::Silence {
+                    message_interpretation = (MessageType::Symbols, Punctuation::None)
+                }
+            }
+        }
+    }
+    match message_interpretation {
+        (MessageType::Shouting, Punctuation::Question) => "Calm down, I know what I'm doing!",
+        (_, Punctuation::Question) => "Sure.",
+        (MessageType::Shouting, _) => "Whoa, chill out!",
+        (MessageType::Silence, _) => "Fine. Be that way!",
+        (_, _) => "Whatever.",
+    }
 }
 
