00af16fe75c8de46553bf15b48aeefeda603f5c8
Ashton Hunger <ahunger@pdx.edu>
2018-04-24 19:04:48 -0700

    exercise complete

--- Project10_exercism_series/src/lib.rs
+++ Project10_exercism_series/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-
+    let mut digits_series: Vec<String> = Vec::new();
+    while digits.len() >= len {
+        digits_series.push(digits.chars().take(len).collect());
+    }
+    digits_series
 }
