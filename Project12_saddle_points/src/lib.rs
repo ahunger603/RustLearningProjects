34efa74303dd3aa55811e6e4bb062474cebcc10e
Ashton Hunger <ahunger@pdx.edu>
2018-04-29 12:05:26 -0700

    refactor answer

--- Project12_saddle_points/src/lib.rs
+++ Project12_saddle_points/src/lib.rs
@@ -1,6 +1,4 @@
-pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut saddle_points: Vec<(usize, usize)> = Vec::new();
-    
+fn get_maxtrix_row_max_col_min(input: &[Vec<u64>]) -> (Vec<u64>, Vec<u64>) {
     let mut row_max: Vec<u64> = Vec::new();
     let mut col_min: Vec<u64> = Vec::new();
     for (i, row) in input.iter().enumerate() {
@@ -21,10 +19,15 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
             }
         }
     }
+    (row_max, col_min)
+}
 
+pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
+    let mut saddle_points: Vec<(usize, usize)> = Vec::new();
+    let row_max_col_min = get_maxtrix_row_max_col_min(input);
     for (i, row) in input.iter().enumerate() {
         for (j, &element) in row.iter().enumerate() {
-            if element <= col_min[j] && element >= row_max[i] {
+            if element <= row_max_col_min.1[j] && element >= row_max_col_min.0[i] {
                 saddle_points.push((i, j));
             }
         }
